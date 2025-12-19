29a25e181b04c8a457996946a793455c2254ee9a
廖沛愉 <yuminous421@gmail.com>
2025-12-19 16:30:32 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
   // 請在此填入您的 Google Apps Script 網址
-  API_ENDPOINT: "https://script.google.com/macros/s/AKfycbw6gMELdUhznQhj5N5XdVLUUlFaSxOSHu-AwvDM-TzEpaTqFyqZ0k9uQuTQWoD9kbxF/exec"
+  API_ENDPOINT: "https://script.google.com/macros/s/AKfycbzAPw6zy9It3JoGgKHeSsMsXECMbfsONAjq5dQ6-pSp-8lorwgeVS3MGd2fP5nhybQG/exec"
 };
+
 
