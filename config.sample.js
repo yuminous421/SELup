630c2d4ab8a3759cc63db4a3d5a14052af0798e1
廖沛愉 <yuminous421@gmail.com>
2025-12-19 14:41:59 +0800

    Add files via upload

---
+++ config.sample.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+  // 請在此填入您的 Google Apps Script 網址
+  API_ENDPOINT: "https://script.google.com/macros/s/AKfycbwaJziQ82TaeCvMmJL6iIoLGiQYk-J5QLnf2tsNjcj_6qJ90hE589TSSvvlZYyc4N0/exec"
+};
